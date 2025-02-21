5380652c27f73481c83b2ae1ffcf1ca5b0bc9f5d
S MUZAMMIL JAVED <s.muzammiljaved@gmail.com>
2025-02-22 02:26:19 +0500

    update

---
+++ react_01/src/components/List.jsx
@@ -0,0 +1,13 @@
+import Item from "./Item";
+
+export default function List(){
+    let arr=["item1","item2","item3","item4"];
+    let item=arr.map((v,index)=>{
+        return <Item key={index} title={v}/>
+    })
+  return (
+    <div>
+     {item}   
+    </div>
+  )
+};

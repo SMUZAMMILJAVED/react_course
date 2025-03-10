c5256f95dde739ec3f3f210361aeb8e80c318a51
S MUZAMMIL JAVED <s.muzammiljaved@gmail.com>
2025-03-11 02:11:20 +0500

    fetch api
    fetch from jsonplaceholder and fetch in useEffects and setdata with the help of use state and renderd into home page and also to string with ? to chnge bool to string

--- react_05/src/pages/Home.jsx
+++ react_05/src/pages/Home.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import HomeContent from '../components/homeContent'
 import { Link } from 'react-router-dom'
 
 const Home = () => {
+
+    const [data,setData]=useState({});
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => setData(json))
+    },[data])
+   
   return (
     <>
-    <div>Home page</div>
+    {/* <div>Home page</div>
     <HomeContent/>
-    <Link to="/about"> go to about page</Link>
+    <Link to="/about"> go to about page</Link> */}
+    <div>
+    title : {data.title} <br />
+    completed : {data.completed?.toString()}
+    </div>
     </>
     
   )

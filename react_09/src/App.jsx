2550cfcbe83a224d7e4d4a55c14c35a11f754dc4
S MuzammiL Javed <121771009+SMUZAMMILJAVED@users.noreply.github.com>
2026-09-11 12:09:33 +0500

    use memo

---
+++ react_09/src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useMemo, useState } from 'react'
+const complexCal=()=>{
+  console.log("working...")
+  let a=0
+  for(let i=0;i<1000;i++){
+a+=i;
+  }
+  return a;
+}
+const App = () => {
+  const [count,setCount]=useState(0)
+  
+  // const result=complexCal()
+  const result =useMemo(()=>{
+   return  complexCal()
+  },[])
+  return (
+    <div>
+      <h1>Counter is {count}</h1>
+      <button onClick={()=>{setCount(count+1)}}>click to update</button>
+    </div>
+  )
+}
+
+export default App
